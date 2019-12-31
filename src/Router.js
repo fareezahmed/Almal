9c65e37f502b67dc80cb5d24958c8f363e379bfa
Fareez Ahmed <fareez1234@gmail.com>
2019-12-31 11:58:21 +1100

    Uncomment router

--- src/Router.js
+++ src/Router.js
@@ -16,43 +16,43 @@ import ContractScreen from './screens/ContractScreen';
 
 const Stack = createStackNavigator(
   {
-    // Splash: {
-    //   screen: SplashScreen,
-    //   navigationOptions: { header: null },
-    // },
-    // Login: {
-    //   screen: LoginScreen,
-    //   navigationOptions: { header: null },
-    // },
-    // SignUp: {
-    //   screen: SignUpScreen,
-    //   navigationOptions: { header: null },
-    // },
-    // ForgotPassword: {
-    //   screen: ForgotPassword,
-    //   navigationOptions: { headerTitle: 'Forgot Password' },
-    // },
+    Splash: {
+      screen: SplashScreen,
+      navigationOptions: { header: null },
+    },
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: { header: null },
+    },
+    SignUp: {
+      screen: SignUpScreen,
+      navigationOptions: { header: null },
+    },
+    ForgotPassword: {
+      screen: ForgotPassword,
+      navigationOptions: { headerTitle: 'Forgot Password' },
+    },
     Main: {
       screen: MainScreen,
       navigationOptions: { header: null },
-      // navigationOptions: ({ navigation }) => ({
-      //   title: 'Main',
-      //   headerTitle: 'Main',
-      //   headerRight: (
-      //     <Button
-      //       large
-      //       onPress={ () => navigation.navigate('Lead') }
-      //       icon={ { name: 'settings', color: Colors.NAV_COLOR } }
-      //       buttonStyle={ {
-      //         marginLeft: 10,
-      //         width: 40,
-      //         height: 40,
-      //         backgroundColor: 'transparent',
-      //       } }
-      //       title=""
-      //     />
-      //   ),
-      // }),
+      navigationOptions: ({ navigation }) => ({
+        title: 'Main',
+        headerTitle: 'Main',
+        headerRight: (
+          <Button
+            large
+            onPress={ () => navigation.navigate('Lead') }
+            icon={ { name: 'settings', color: Colors.NAV_COLOR } }
+            buttonStyle={ {
+              marginLeft: 10,
+              width: 40,
+              height: 40,
+              backgroundColor: 'transparent',
+            } }
+            title=""
+          />
+        ),
+      }),
     },
     Settings: {
       screen: SettingsScreen,
